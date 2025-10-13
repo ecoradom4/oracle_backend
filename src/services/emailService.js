0b20e1cf723a864eba0a56c6f86a23326736bfc8
Eduardo <eduardo@debian.eduardo>
2025-10-13 14:18:14 -0600

    servicio correo

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -6,7 +6,9 @@ const fs = require('fs').promises;
 class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
